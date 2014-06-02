25ad2b72f06c60cdc1b2dea8713bfe8d75a45809
joshjeong <jcjeong@gmail.com>
2014-06-01 22:32:28 -0700

    Update my_solution.js

--- week_8_and_9/2_JavaScript/translate_to_js/my_solution.js
+++ week_8_and_9/2_JavaScript/translate_to_js/my_solution.js
@@ -1,7 +1,7 @@
 // U3.W8-9: Challenge you're converting
 
 
-// I worked on this challenge [by myself, with: ].
+// I worked on this challenge [by myself].
 
 // 2. Pseudocode
 
@@ -53,13 +53,12 @@ var PezDispenser = function(pez) {
     console.log("You got flavor " + pezDispensed);
     this.allFlavors.splice(this.allFlavors.indexOf(pezDispensed), 1 );
 
-
-  //   this.allFlavors.pop(Math.floor(Math.random()* this.allFlavors.length) + 1)
-  // }
 }
 
 }
 
+
+// Driver Code
 pez = new PezDispenser('pez');
 pez.add("orange");
 pez.add("red");
@@ -70,33 +69,8 @@ pez.getPez();
 pez.seeAllPez();
 
 /*
-# 4. Refactored Solution
-# Code already seems pretty DRY. I couldn't figure out how to make get_pez a litte cleaner.
-
-# 1. DRIVER TESTS GO BELOW THIS LINE
-
-flavors = %w(cherry chocolate cola grape lemon orange peppermint raspberry strawberry).shuffle
-super_mario = PezDispenser.new(flavors)
-puts "A new pez dispenser has been created. You have #{super_mario.pez_count} pez!"  
-puts "Here's a look inside the dispenser:"  
-puts super_mario.see_all_pez 
-puts "Adding a purple pez."
-super_mario.add_pez("purple")   # mmmmm, purple flavor
-puts "Now you have #{super_mario.pez_count} pez!"
-puts "Oh, you want one do you?"
-puts "The pez flavor you got is: #{super_mario.get_pez}"
-puts "Now you have #{super_mario.pez_count} pez!"
-*/ 
-
-
-
-
-
-// 1. DRIVER TESTS/ASSERT STATEMENTS GO BELOW THIS LINE
-
-
-
 
 
 
 // 5. Reflection 
+// This challenge ended up being extremely similar to the grocery list. Other than that, not much to say.
